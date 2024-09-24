4fd0ba3692439427df2dd38c6c552eb8106d5417
sahabulislamsifat <sahabulislamsifat@gmail.com>
2024-09-25 03:34:18 +0600

    git commit

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,8 +3,9 @@ const balanceBtn = document.getElementById("available-balance");
 const donateBtn = document.getElementById("btn-donate");
 const inputAmount = document.getElementById("input-amount");
 const updateBalance = document.getElementById("donated-amount");
-
-donateBtn.addEventListener("click", function () {
+// Donation Template  1
+donateBtn.addEventListener("click", function (event) {
+  event.preventDefault();
   const currentBalance = parseFloat(balanceBtn.innerText);
   const donationAmount = parseFloat(inputAmount.value);
   const updateBalanceValue = parseFloat(updateBalance.innerText);
@@ -22,10 +23,36 @@ donateBtn.addEventListener("click", function () {
   updateBalance.innerText = updateBalanceValue + donationAmount;
 
   balanceBtn.innerText = newBalance.toFixed(2);
+  my_modal_1.showModal();
   inputAmount.value = "";
 });
 
-// History Button
+// Donation Template  2
+const balanceBtn2 = document.getElementById("btn-donate-2");
+balanceBtn2.addEventListener("click", function () {
+  const currentBalance = parseFloat(balanceBtn.innerText);
+  const donationAmount = parseFloat(inputAmount.value);
+  const updateBalanceValue = parseFloat(updateBalance.innerText);
+  console.log(donationAmount)
+
+  if (
+    isNaN(donationAmount) ||
+    donationAmount < 0 ||
+    donationAmount >= currentBalance
+  ) {
+    alert("invalid Donation Amount");
+    return;
+  }
+
+  const newBalance = currentBalance - donationAmount;
+  updateBalance.innerText = updateBalanceValue + donationAmount;
+
+  balanceBtn.innerText = newBalance.toFixed(2);
+  my_modal_1.showModal();
+  inputAmount.value = "";
+});
+
+// History Button Toggle
 document.getElementById("history-btn").addEventListener("click", function () {
   document.getElementById("main-card-section").classList.add("hidden");
 });
---
+++ JavaScript/utilities.js
@@ -0,0 +1,4 @@
+// const balanceBtn = document.getElementById("available-balance");
+// const donateBtn = document.getElementById("btn-donate");
+// const inputAmount = document.getElementById("input-amount");
+// const updateBalance = document.getElementById("donated-amount");
